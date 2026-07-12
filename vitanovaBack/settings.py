a62f206ba80e82199e3b564c99df793bba7cd222
Engineer Sam <sammyyawsnana1@gmail.com>
2026-07-12 19:36:42 +0000

    Project package requirement installation

--- vitanovaBack/settings.py
+++ vitanovaBack/settings.py
@@ -38,6 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     "vitanovaAPI",
+    'rest_framework',
+      "rest_framework_simplejwt",
+    "corsheaders",
+
 ]
 
 MIDDLEWARE = [
@@ -122,3 +126,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+#CORS ALLOWING PERMISSON ACCESS
+CORS_ALLOW_ALL_ORIGINS = True
+AUTH_USER_MODEL = 'vitanovaAPI.CustomUser'
